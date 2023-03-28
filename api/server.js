d8b7f71c4b78775b9fbd5798f9018bfd4758de41
ar-ritik.suneja <ritiks12345678@gmail.com>
2023-03-28 20:09:22 +0530

    changes for hosting on versel

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 const jsonServer = require("json-server");
 const cors = require("cors");
 const server = jsonServer.create();
-const router = jsonServer.router("./db.json");
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
